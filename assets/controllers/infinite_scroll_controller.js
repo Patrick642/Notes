d58d34efe6cd409296305b63dfd1d77ee14fed7a
Patrick642 <109094173+Patrick642@users.noreply.github.com>
2024-08-02 15:59:06 +0200

    Update
    - Change the way infinite scroll is triggered, by scrolling to the bottom of the page, not via a button

--- assets/controllers/infinite_scroll_controller.js
+++ assets/controllers/infinite_scroll_controller.js
@@ -3,31 +3,58 @@ import Masonry from 'masonry-layout';
 import $ from 'jquery';
 
 export default class extends Controller {
-    load() {
-        $('#infiniteScrollButton').prop('disabled', true);
+    loading = false;
+    isLast = ($('#infiniteScrollData').length === 0) ? true : false;
+
+    connect() {
+
+        // In case if the page loads when a user has already scrolled to the bottom of the page, or if they have a large monitor (so there is no scrollbar and they cannot scroll), load more bookmarks.
+        this.#checkScrollAndLoadData();
+    }
+
+    scroll() {
+        this.#checkScrollAndLoadData();
+    }
+
+    // Load more bookmarks and append it.
+    #loadMoreData() {
+        const self = this;
+
+        $('#infiniteScrollSpinner').removeClass('d-none');
+        this.loading = true;
         let offset = $('.note-container').length;
 
         $.ajax({
-            type: "GET",
-            url: $('#infiniteScrollButton').data('url') + "?offset=" + offset,
+            type: 'GET',
+            url: $('#infiniteScrollData').data('url'),
+            data: {
+                offset: offset
+            },
             cache: false,
             success: function (out) {
                 if (out.success) {
                     $('.row.notes-container').append(out.render);
+                    self.loading = false;
+                    if (out.isLast)
+                        self.isLast = true;
 
-                    // Remove 'Load more notes' button after loading all user's notes
-                    if (out.isLast) {
-                        $('#infiniteScrollButton').closest('.col').remove();
-                    }
+                    self.#checkScrollAndLoadData();
                 }
 
-                // Recollects all container elements
+                // Recollects all container elements.
                 new Masonry(document.querySelector('.row.notes-container'), {
                     itemSelector: '.note-container'
                 }).reloadItems();
 
-                $('#infiniteScrollButton').prop('disabled', false);
+                $('#infiniteScrollSpinner').addClass('d-none');
             }
         });
+    }
+
+    // Check if a user cannot scroll, if loading is not already in progress, and if a user has more bookmarks.
+    #checkScrollAndLoadData() {
+        if (Math.ceil($(window).scrollTop() + $(window).height()) >= $(document).height() && !this.loading && !this.isLast) {
+            this.#loadMoreData();
+        }
     }
 }
